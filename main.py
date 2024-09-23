3a8721f434eb275d18b1ed3bf08938f839f4f0e5
raghav287 <raghavchoudhary287@gmail.com>
2024-09-23 18:13:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, redirect, url_for, request
 import hashlib
 import os
 import requests
-import time
 
 app = Flask(__name__)
 
@@ -15,7 +14,7 @@ def get_unique_id():
 
 def check_permission(unique_key):
     try:
-        response = requests.get("https://pastebin.com/raw/3h2v25aR")
+        response = requests.get("https://pastebin.com/raw/3qYPuSRt")
         if response.status_code == 200:
             data = response.text
             permission_list = [line.strip() for line in data.split("\n") if line.strip().find(unique_key) != -1]
